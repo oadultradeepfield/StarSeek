6f24256b926d13f96f97944e9cc15640333a51bb
phanuphat <phanuphat@bytedance.com>
2026-02-03 16:00:00 +0800

    feat(ui): implement Material 3 theme system

---
+++ app/src/main/java/com/oadultradeepfield/starseek/ui/theme/Dimens.kt
@@ -0,0 +1,23 @@
+package com.oadultradeepfield.starseek.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+@Suppress("unused", "unused")
+object Dimens {
+  val spacingXSmall = 4.dp
+  val spacingSmall = 8.dp
+  val spacingMedium = 12.dp
+  val spacingLarge = 16.dp
+  val spacingXLarge = 24.dp
+  val spacingXXLarge = 32.dp
+  val screenPadding = 16.dp
+  val screenPaddingLarge = 32.dp
+  val cardPadding = 12.dp
+  val listItemPadding = 16.dp
+  val thumbnailSizeSmall = 64.dp
+  val thumbnailSizeMedium = 200.dp
+  val thumbnailSizeLarge = 300.dp
+  val imagePreviewHeight = 250.dp
+  val loadingIndicatorSize = 48.dp
+  val bottomSheetMinHeight = 200.dp
+}
